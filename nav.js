36b121b1e6977aa6d9eb35aab661bd6f59e9b85f
jaidenmuruvan <2726725@students.wits.ac.za>
2025-11-04 01:38:43 +0200

    test

--- nav.js
+++ nav.js
@@ -1,28 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // Detect if we're in a subfolder (like Stats/, ItemShop/, etc.)
-  const isInSubfolder = window.location.pathname.split("/").length > 2;
-  const basePath = isInSubfolder ? "../" : "";
-
   const navHTML = `
     <nav class="main-nav">
       <div class="nav-container">
-        <a href="${basePath}index.html" class="logo">Fortnite Hub</a>
+        <a href="index.html" class="logo">Fortnite Hub</a>
 
         <button class="menu-toggle" id="menu-toggle" aria-label="Toggle Menu">
           ☰
         </button>
 
         <ul class="navigation-menu" id="nav-menu">
-          <li><a href="${basePath}../index.html">Home</a></li>
-          <li><a href="${basePath}../Stats/stats.html">Stats</a></li>
-          <li><a href="${basePath}../ItemShop/itemShop.html">Item Shop</a></li>
-          <li><a href="${basePath}../GameModes/gameModes.html">Game Modes</a></li>
+          <li><a href="index.html">Home</a></li>
+          <li><a href="Stats/stats.html">Stats</a></li>
+          <li><a href="ItemShop/itemShop.html">Item Shop</a></li>
+          <li><a href="GameModes/gameModes.html">Game Modes</a></li>
           <li class="dropdown">
             <a href="#">Cosmetics ▼</a>
             <ul class="dropdown-menu">
-              <li><a href="${basePath}../Outfit/cosmetics.html">All Cosmetics</a></li>
-              <li><a href="${basePath}../Outfit/locker.html">Your Locker</a></li>
-              <li><a href="${basePath}../Outfit/outfit.html">Outfit Randomizer</a></li>
+              <li><a href="Outfit/cosmetic.html">All Cosmetics</a></li>
+              <li><a href="Outfit/locker.html">Your Locker</a></li>
+              <li><a href="Outfit/outfit.html">Outfit Randomizer</a></li>
             </ul>
           </li>
         </ul>
@@ -30,20 +26,19 @@ document.addEventListener("DOMContentLoaded", () => {
     </nav>
   `;
 
-  // Insert navigation
   document.body.insertAdjacentHTML("afterbegin", navHTML);
 
-  // Mobile toggle functionality
+  // Mobile toggle
   const menuToggle = document.getElementById("menu-toggle");
   const navMenu = document.getElementById("nav-menu");
   menuToggle.addEventListener("click", () => {
     navMenu.classList.toggle("show-menu");
   });
 
-  // Highlight current active page
+  // Highlight active page
   const currentPage = window.location.pathname.split("/").pop();
   document.querySelectorAll(".navigation-menu a").forEach(link => {
-    if (link.getAttribute("href").includes(currentPage) && currentPage !== "") {
+    if (link.getAttribute("href").endsWith(currentPage)) {
       link.classList.add("active");
     }
   });
